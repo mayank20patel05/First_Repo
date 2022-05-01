278b88a8ec847e7769f1ae4ebd4dbd2fbdf20211
Mayank Patel <95314662+mayank20patel05@users.noreply.github.com>
2022-05-01 17:30:32 -0400

    second if statement is added.

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,9 +5,13 @@ public class HelloWorld{
 		System.out.println("Hello Mayank!");
 		System.out.println("Hello Java!");
 		int i = 0;
+		int count = 0;
 		
 		if (i == 0) {
-			System.out.println("My name is mayank");
+			System.out.println("My name is if");
+		}
+		if (count == 0) {
+			System.out.println("My name is zero");
 		}
 	}		
 }
